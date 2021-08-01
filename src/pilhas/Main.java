a9a6eb19f4e254d45b9c1d9b557b9c97b88cb925
Giovanne Russo <russogiovanne@gmail.com>
2021-08-01 15:06:35 -0300

    Implementing a Stack

---
+++ src/pilhas/Main.java
@@ -0,0 +1,27 @@
+package pilhas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack();
+
+        myStack.push(new Node(1));
+        myStack.push(new Node(2));
+        myStack.push(new Node(3));
+        myStack.push(new Node(4));
+        myStack.push(new Node(5));
+
+        System.out.println(myStack);
+
+        System.out.println(myStack.pop());
+        System.out.println(myStack);
+
+        myStack.push(new Node(6));
+        System.out.println(myStack);
+
+        System.out.println();
+        System.out.println(myStack.top());
+
+    }
+}
---
+++ src/pilhas/Stack.java
@@ -0,0 +1,55 @@
+package pilhas;
+
+public class Stack {
+
+    private Node refNodeEntryStack;
+
+    public Stack() {
+        this.refNodeEntryStack = null;
+    }
+
+    public void push(Node newNode) {
+        Node refAux = refNodeEntryStack;
+        refNodeEntryStack = newNode;
+        refNodeEntryStack.setRefNode(refAux);
+    }
+
+    public Node pop() {
+        if (!isEmpty()) {
+            Node poppedNode = refNodeEntryStack;
+            refNodeEntryStack = refNodeEntryStack.getRefNode();
+            return poppedNode;
+        }
+        return null;
+    }
+
+    public Node top() {
+        return refNodeEntryStack;
+    }
+
+    public boolean isEmpty() {
+        return refNodeEntryStack == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String returnString = " ------------\n";
+        returnString += "     Stack\n";
+        returnString += " ------------\n";
+
+        Node auxNode = refNodeEntryStack;
+
+        while (true) {
+            if (auxNode != null) {
+                returnString += "[Node{data=" + auxNode.getData() + "}]\n";
+                auxNode = auxNode.getRefNode(); //Recebe a referência do Próximo Nó, passando a ser o próximo Nó
+            }
+            else {
+                break;
+            }
+        }
+        returnString += " ============\n";
+        return returnString;
+    }
+
+}
